8976309471d5b167f8ed609d45710fdef9b43910
rstina <53047495+rstina@users.noreply.github.com>
2020-01-31 15:44:59 +0100

    renamed products

--- script.js
+++ script.js
@@ -9,5 +9,23 @@ let dispDiv = document.getElementById("dispProd")
 // price
 // dropdown to choose from
 // submit button  - add product to basket -eventlistner 
+fetch('protucts.json')
+.then(prods => console.log(prods))
+
+function showProducts(user){
+  for(let i = 0; i < user.length; i++){
+
+  let name = user[i].name
+  let city = user[i].address.city
+  document.getElementById("dispProd").innerHTML += 
+  "<div class='card' style='width: 15rem;' >" +
+    "<h2>" + 
+  name + 
+    "</h2>" +
+    "<p>" +
+  city + "</p></div><br>"
+  }
+}
+
 
 // product basket
