1152de0f26de850b5480df22515e263991327174
JingJing Lin <jlina.lin@gmail.com>
2020-02-04 16:05:38 +0100

    added cart first part

--- basket.js
+++ basket.js
@@ -7,10 +7,30 @@ fetch( 'products.json' )
 .then( names => getNames( names ) )
 
 function getNames (names){
-  for(let i = 0; i < names.products.length; i++){
-    let name = names.products[i].name
+    dispCart.innerHTML += 
+    "<tr>"+
+        "<th>Name</th>"+
+        "<th>Amount</th>"+
+         "<th>Price</th>"+
+    "</tr>"
+
+    for(let i = 0; i < localStorage.length; i++){
+    let name = localStorage.getItem( localStorage.key( i ) )
+    let price =names.products[i].price
+    // let amount = localStorage.key(i).value
+
+    console.log(localStorage.getItem( localStorage.key( i ) ))
+
     // hämtar värdet
     localStorage.getItem( name )
-    console.log("Name: " + name + " Tickets: " + localStorage.getItem( name ))
+    // console.log("Name: " + name + " Tickets: " + localStorage.getItem( name ))
+
+    dispCart.innerHTML += 
+    "<tr>"+
+        "<td>"+name+"</td>"+
+        // "<td>"+amount+"</td>"+
+        "<td>"+price+"</td>"+
+    "</tr>" 
   }
+
 }
