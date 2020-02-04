7c332a30ef38d0f5a2fe86266840a513001b4c0d
rstina <53047495+rstina@users.noreply.github.com>
2020-02-04 16:05:58 +0100

    local storage saved

--- display-products.js
+++ display-products.js
@@ -2,16 +2,38 @@
 let body = document.querySelector("body")
 let dispDiv = document.getElementById("dispProd")
 
-// number of tickets ordered
+if( localStorage.length == 0 ) {
+  console.log("storage empty")
+} else {
+  console.log("storage exist");
+  console.table( localStorage )
+}
 
+// fetch( 'products.json' )
+// .then( storage => storage.json() )
+// .then( storage => getNumbers( storage ) )
+// function getNumbers (storage){
+//   let totTickets = 0
+//   for(let i = 0; i < storage.products.length; i++){
+//     let name = storage.products[i].name
+//     totTickets += parseInt(localStorage.getItem( name ))
+//     console.log(name +" " + parseInt(localStorage.getItem( name )))
+//   }
+//   console.log("Total number of tickets: " + totTickets)
+//   if(totTickets < 13 ){
+//     console.log("Tot: " + totTickets)
+//   } else {
+//     console.log("More that 12 tickets: " + totTickets)
+//   }
+// }
+console.log()
+localStorage.key
 // display products
 // dropdown to choose from
 // submit button  - add product to basket -eventlistner 
 fetch( 'products.json' )
 .then( prods => prods.json() )
 .then( prods => showProducts( prods ) )
-
-console.table(localStorage)
 
 function showProducts( products ){
   for(let i = 0; i < products.products.length; i++){
@@ -20,10 +42,6 @@ function showProducts( products ){
   let description = products.products[i].description
   let price = products.products[i].price
   let prodId = products.products[i].id
-  let amount = products.products[i].amount
-
-  // local storage - ersätt med en function istället?
-  localStorage.setItem( name, amount )
   
   // create div for each product with product info, select and submitbtn
   dispDiv.innerHTML += 
@@ -46,6 +64,10 @@ function showProducts( products ){
   addListnerToBtn()
 }
 
+  // local storage - ersätt med en function istället?
+  // localStorage.setItem( name, amount )
+
+
 // vi behöver en funktion som skickar value till basket
 // sätta eventLister på formuläret istället? För att lättare komma åt informationen inom this
 // gör en addEventListner som reagerar på knapp tryck
@@ -54,44 +76,43 @@ function addListnerToBtn(){
   allBtns.forEach( btn => {
     btn.addEventListener('click',function(){ 
       // get current name
-      let currentName = this.parentElement.children[1].innerHTML
+      let eName = this.parentElement.children[1].innerHTML
 
       // get current value
-      let currentSelectVal = this.parentElement.children[4].value
-      currentSelectVal = parseInt(currentSelectVal)      
+      let eSelectVal = this.parentElement.children[4].value
+      eSelectVal = parseInt(eSelectVal)
+      console.log(typeof(eSelectVal));
+      
 
       // get current price
-      let currentPrice = this.parentElement.children[3].value
-      currentPrice = parseInt(currentPrice)
+      let ePrice = this.parentElement.children[3].innerHTML
+      ePrice = parseInt(ePrice)
 
       // current amount of tickets
-      let currentAmount = localStorage.getItem(currentName)
-      currentAmount = parseInt(currentAmount)
-      typeof(currentAmount)
+      let eTickets = localStorage.getItem(eName)
+      eTickets = parseInt(eTickets)
 
       // feedback that you've added products to basket
-      // console.log("Added " + currentSelectVal + " " + currentName + "-tickets to basket ")
+      console.log("Added " + eSelectVal + " " + eName + "-tickets to basket ")
       
+
       // either set an amount for the first time or add together with previous amount
       // it's a string currently and must be a number
-      if( currentAmount == 0 ){
-        console.log("amount was empty")
-        localStorage.setItem( currentName, currentSelectVal )
-        console.log("set new amount to: " + localStorage.getItem(currentName));
+      if( eTickets == null || eTickets == 0 ){
+        console.log(eTickets)        
+        localStorage.setItem( eName, [eSelectVal, ePrice, eSelectVal*ePrice] )
+        console.log("Set first amount to: " + localStorage.getItem(eName))
       } else {
-        localStorage.setItem( currentName, currentSelectVal + currentAmount )
-        console.log("new amount: " + localStorage.getItem(currentName) )
+        let eNewTicketSum = eSelectVal + eTickets
+        localStorage.setItem( eName, [eNewTicketSum, ePrice, eNewTicketSum*ePrice] )
+        console.log("Change new amount to: " + localStorage.getItem(eName) )
       }
       
       // if(localStorage.getItem( currentName, currentSelectVal ))
-      console.log(localStorage.getItem(currentName))
+      // console.log(localStorage.getItem(currentName))
       
       // reset button value
       this.parentElement.children[4].value  = 0
-
-      console.table(localStorage)
     })
   })
 }
-
-console.log(localStorage.getItem("Bali"))
