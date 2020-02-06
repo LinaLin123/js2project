82b271cd98b7045904a8bc6216370084ec37158c
rstina <53047495+rstina@users.noreply.github.com>
2020-02-06 07:50:50 +0100

    buttons connected

--- cart.js
+++ cart.js
@@ -1,64 +1,58 @@
 // connect to HTML-document
 let dispCart = document.getElementById("dispCart")
-let test = document.getElementById("test")
-// display cart
-
-// Hämta data från localStorage
-let cart = JSON.parse(localStorage.getItem('Name')) 
-// console.table( localStorage )
-
-//Visa alla nycklar
-// for (let i = 0; i < localStorage.length; i++) {
-//   console.log("Key: " + localStorage.key(i));
-//   }
 
 // https://github.com/mahmudalhakim/JavaScript_2_FEND19/blob/master/Lektion_06/localstorage/localstorage-demo.html
 // https://studentportal.nackademin.se/pluginfile.php/171567/mod_resource/content/0/JavaScript-2-Lektion-06.pdf
 
-fetch( 'products.json' )
-.then( products => products.json() )
-.then( products => getProdsToCart( products ) )
+// get data from .json
+function getProducts(){
+  fetch( 'products.json' )
+  .then( products => products.json() )
+  .then( products => getProdsToCart( products ) )
+}
+getProducts()
 
+// display cart
 function getProdsToCart (products){
+  dispCart.innerHTML = ""
   ifEmptyCart() // either writes empty cart or table heading
-  let totalSum = 0;
+  let totalSum = 0
 
   for( let i = 0; i < localStorage.length; i++ ){
-    let id = localStorage.key( i )
+    let id = localStorage.key(i)
     let name = products[id].name
-    let tickets = localStorage.getItem( localStorage.key( i ) )
-    tickets = parseInt(tickets)
-    console.log()
+    let tickets = parseInt( localStorage.getItem( localStorage.key(i) ) )
     let price = products[id].price
     let sum = tickets * price
     totalSum += sum
-    // hämtar värdet
-    
-    console.log(localStorage.getItem( id ))
-    // console.log("Name: " + name + " Tickets: " + localStorage.getItem( name ))
 
     dispCart.innerHTML +=
-    "<tr class='table-row'>" +
+    "<tr id='" + id + "' class='table-row'>" +
       "<td><button id ='" + id + "deleteBtn' class='btn btn-warning btn-sm'>x</button></td>" +
       "<td>" + name + "</td>" +
-      "<td><button id='" + id + "BtnMinus' class='minus btn btn-primary btn-sm'>-</button>" +
+      "<td><button id='" + id + "MinusBtn' class='minus btn btn-info btn-sm'>-</button>" +
       "<input type='number' id='"+id+"Input' class='inputAmount' value='" + tickets + "'></input>" +
-      "<button id='" + id + "BtnPlus'  class='plus btn btn-primary btn-sm'>+</button></td>" +
+      "<button id='" + id + "PlusBtn'  class='plus btn btn-info btn-sm'>+</button></td>" +
       "<td>" + price + "</td>" +
       "<td>" + sum + "</td>" +
     "</tr>" 
   }
+  // display table footer with total sum
   dispCart.innerHTML +=
-  "<thead><tr class='table-row thead-dark'>" +
+  "<thead><tr class='table-row thead'>" +
     "<th></th>" +
     "<th></th>" +
     "<th></th>" +
     "<th>Total sum: </th>" +
-    "<th>" + totalSum + "</th>" + 
-  "</tr></thead>" + 
-  "<tr><td>Name: <input type='text' placeholder='Name...'></input></td>"
-    + "<td>Email: <input type='text' placeholder='Email...'></input></td>"
-    +"<td></td><td></td><td><button id='orderBtn' class='btn btn-success'>Place order</button></td></tr>"
+    "<th>" + totalSum + "</th></tr></thead>"
+  // display order button and contact info if items in cart
+  if( localStorage.length > 0){
+    dispCart.innerHTML +=
+    "<thead class='thead thead-dark'><tr><th><input type='text' placeholder='Name...'></input></th>" +
+     "<th><input type='email' placeholder='Email...'></input></th>" +
+     "<th></th><th></th>" +
+    "<th><button id='orderBtn' class='btn btn-success'>Place order</button></th></tr></thead>"
+  }
   inputChange()
   getBtns()
 }
@@ -68,37 +62,68 @@ function ifEmptyCart(){
     document.getElementById("emptyCart").innerHTML = ""
     dispCart.innerHTML += 
     "<thead class='thead thead-dark'><tr>" +
-        "<th><button id='clearCartBtn' class='btn btn-danger btn-sm deletAllBtn'>Clear cart</button></th>"+
-        "<th>Destination</th>"+
-        "<th>Tickets</th>"+
-         "<th>Price per ticket</th>"+
-         "<th>Sum</th>"
+      "<th><button id='clearCartBtn' class='btn btn-warning btn-sm deletAllBtn'>Clear cart</button></th>"+
+      "<th>Destination</th>"+
+      "<th>Tickets</th>"+
+      "<th>Price per ticket</th>"+
+      "<th>Sum</th>"
     "</tr></thead>"
   } else {
     document.getElementById("emptyCart").innerHTML = "Your cart is empty!"
   }
 }
 
-
+// should change local storage if there's a change of numer of items
 function inputChange(){
   let inputs = document.querySelectorAll("input")
   inputs.forEach( input => {
     input.addEventListener('change',function(e){ 
       console.log(this)
+      console.log(this.id)
     })
   })
 }
 
 function getBtns(){
-  let removeBtn = document.querySelectorAll("button")
-  removeBtn.forEach( input => {
-    input.addEventListener('click',function(e){ 
-      if(this.id == "clearCartBtn"){
+  let allCartBtns = document.querySelectorAll("button")
+  allCartBtns.forEach( btn => {
+    btn.addEventListener('click',function(e){ 
+      let eId = this.id
+      let eParentId = this.parentElement.parentElement.id
+      // clear cart
+      if( eId == "clearCartBtn"){
         alert("Are you sure you would like to delete all items in your cart?")
-        console.log("Clearing the cart!")
+        localStorage.clear()
+        getProducts()
+      } // order 
+      else if( eId == "orderBtn"){
+        alert("ORDERED!")
+      } // delete item
+      else if( eId == (eParentId+"deleteBtn") ){
+        document.getElementById(eParentId).remove() // remove row
+        localStorage.removeItem(eParentId) // remove from local storage
+        getProducts()
+      } // remove one item
+      else if ( eId == eParentId+"MinusBtn" ){
+        if( parseInt(this.nextElementSibling.value) > 0 ){
+          console.log("removing one ticket")
+          localStorage.setItem(eParentId,(parseInt(this.nextElementSibling.value)-1))
+          getProducts()
+        }
+      } // add item
+      else if ( eId == eParentId+"PlusBtn" ){
+        console.log("adding one ticket")
+        localStorage.setItem(eParentId,(parseInt(this.previousSibling.value)+1))
+        getProducts()
       } else {
-        console.log(this)
+        console.log(eId)
       }
     })
   })
+}
+
+function localStorageChange() {
+  for( let i = 0; i < localStorage.length; i++ ){
+
+  }
 }
