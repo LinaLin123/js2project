9c7bd3bc01f888986f12df0fc86f7f9f86c8ca81
JingJing Lin <jlina.lin@gmail.com>
2020-02-06 15:31:57 +0100

    add confirmation form

---
+++ confirmation.js
@@ -0,0 +1,48 @@
+let dispOrder = document.getElementById("dispOrder")
+
+function getProducts(){
+    fetch( 'products.json' )
+    .then( products => products.json() )
+    .then( products => getcartToOrderForm( products ) )
+  }
+  getProducts()
+  
+  // display confirmation
+  function getcartToOrderForm (products){
+    let totalSum = 0
+    for( let i = 0; i < localStorage.length; i++ ){
+      let id = localStorage.key(i)
+      let name = products[id].name
+      let tickets = parseInt( localStorage.getItem( localStorage.key(i) ) )
+      let price = products[id].price
+      let sum = tickets * price
+      totalSum += sum
+  
+      dispOrder.innerHTML +=
+      "<tr id='" + id + "' class='table-row'>" +
+        "<td></td>" +
+        "<td>" + name + "</td>" +
+        "<td>" + tickets +"</td>" +
+        "<td>" + price + "</td>" +
+        "<td>" + sum + "</td>" +
+      "</tr>" 
+    }
+    dispOrder.innerHTML +=
+    "<thead><tr class='thead thead-dark'>" +
+      "<th></th>" +
+      "<th>Destination</th>" +
+      "<th>Tickets</th>" +
+      "<th>Price/ticket</th>" +
+      "<th></th></tr></thead>"
+
+dispOrder.innerHTML +=
+    "<thead><tr class='thead thead-dark''>" +
+      "<th></th>" +
+      "<th></th>" +
+      "<th></th>" +
+      "<th>Total sum: </th>" +
+      "<th>" + totalSum + "</th></tr></thead>"
+  }
+
+  
+  
