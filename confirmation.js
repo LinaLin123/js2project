d1e970bc378d77e5f0027eacad07a9de83010a02
rstina <53047495+rstina@users.noreply.github.com>
2020-02-10 12:35:38 +0100

    updated confirmation with comments

--- confirmation.js
+++ confirmation.js
@@ -1,17 +1,33 @@
+// 1. connect to HTML-document
 let dispOrder = document.getElementById("dispOrder")
 
+// 2. get product information from json-file
+getProducts()
+
+// 2. get product information from json-file and send to getcartToOrderForm-function
 function getProducts() {
   fetch('products.json')
     .then(products => products.json())
     .then(products => getcartToOrderForm(products))
 }
 
-getProducts()
-
-// display confirmation
+// 3. display confirmation page with items from local storage in a table
 function getcartToOrderForm(products) {
+  // 3.1 add table header
+  dispOrder.innerHTML +=
+    "<thead><tr class='thead thead-dark'>" +
+    "<th></th>" +
+    "<th>Destination</th>" +
+    "<th>Tickets</th>" +
+    "<th>Price/ticket</th>" +
+    "<th>Sum</th></tr></thead>"
+
+  // 3.2 initalize totalSum
   let totalSum = 0
+
+  // 3.3 loop over local storage to display cart products
   for (let i = 0; i < localStorage.length; i++) {
+    // 3.3.1 initalizes values needed to display products
     let id = localStorage.key(i)
     let name = products[id].name
     let tickets = parseInt(localStorage.getItem(localStorage.key(i)))
@@ -19,6 +35,7 @@ function getcartToOrderForm(products) {
     let sum = tickets * price
     totalSum += sum
 
+    // 3.3.2 display items in table
     dispOrder.innerHTML +=
       "<tr id='" + id + "' class='table-row'>" +
       "<td></td>" +
@@ -28,14 +45,7 @@ function getcartToOrderForm(products) {
       "<td>" + sum + "</td>" +
       "</tr>"
   }
-  dispOrder.innerHTML +=
-    "<thead><tr class='thead thead-dark'>" +
-    "<th></th>" +
-    "<th>Destination</th>" +
-    "<th>Tickets</th>" +
-    "<th>Price/ticket</th>" +
-    "<th>Sum</th></tr></thead>"
-
+  // 3.4 display table footer with total sum 
   dispOrder.innerHTML +=
     "<thead><tr class='thead thead-dark''>" +
     "<th></th>" +
@@ -43,10 +53,11 @@ function getcartToOrderForm(products) {
     "<th></th>" +
     "<th>Total sum: </th>" +
     "<th>" + totalSum + "</th></tr></thead>"
-
-clearCart()
+  // 4. After displaying items, clear cart
+  clearCart()
 }
 
+// 4. clear local storage
 function clearCart(){
-    localStorage.clear()
+  localStorage.clear()
 }
